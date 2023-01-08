c5afbbc07058448f2f94344c65c0393f939de249
quiet-pirate <ale.alducin@gmail.com>
2023-01-07 21:49:07 -0700

    completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string grade = Console.ReadLine();
+        int percentage = int.Parse(grade);
+
+        string letter = "";
+
+        if (percentage >=90)
+        {
+            letter = "A";
+        }
+        else if (percentage >=80)
+        {
+            letter = "B";
+        }
+        else if (percentage >=70)
+        {
+            letter = "C";
+        }
+        else if (percentage >=60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine ($"Your grade is: {letter}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying! You'll get it next time.");
+        }
     }
 }
