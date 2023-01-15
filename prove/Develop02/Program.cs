28e21b322dfd1da6c9217d4211afdfaafa9da506
quiet-pirate <ale.alducin@gmail.com>
2023-01-14 22:50:28 -0700

    completed

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,40 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+namespace Developer02{
+    public class Program
     {
-        Console.WriteLine("Hello Develop02 World!");
+        static void Main()
+        {
+            Journal journal = new Journal();
+            int _choice = 0;
+            Console.WriteLine("Please choose what you would like to do:\n 1.Write\n 2.Display\n 3.Load File\n 4.Save\n 5.Quit\n 6.Add Photo");
+            _choice = int.Parse(Console.ReadLine());
+
+            if(_choice == 1)
+            {
+                journal.WriteEntry();
+            }
+            if(_choice == 2)
+            {
+                 journal.Display();
+            }
+            if(_choice == 3)
+            {
+                journal.LoadFile();
+            }
+            if(_choice == 4)
+            {
+                journal.SaveFile();
+            }
+            if(_choice == 5)
+            {
+                journal.Quit();
+            }
+            if(_choice == 6)
+            {
+                journal.Photo();
+            }
+        }
     }
 }
+
